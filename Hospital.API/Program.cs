d396fcae13149d8aa438ad1dd22ef8b5a1b7b5d6
kuzda <dmitriy.kuznetsov@elesy.ru>
2024-09-05 00:28:54 +0600

    add: disign time context registration

--- Hospital.API/Program.cs
+++ Hospital.API/Program.cs
@@ -1,21 +1,36 @@
+using Hospital.API.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(x =>
+{
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<ApiMainContext>(opt =>
+    {
+        opt.UseInMemoryDatabase("disigntime.db");
+    }); 
+}
+else
+{
+    //Ms SQL
+
+}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
