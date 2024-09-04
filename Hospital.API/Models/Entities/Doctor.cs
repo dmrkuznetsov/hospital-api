d4724cf0253f4fb8dcdae314e067e5f7c84826c9
kuzda <dmitriy.kuznetsov@elesy.ru>
2024-09-05 01:27:25 +0600

    fix: misspelled 'c' in russian

--- Hospital.API/Models/Entities/Doctor.cs
+++ Hospital.API/Models/Entities/Doctor.cs
@@ -1,6 +1,6 @@
 ﻿namespace Hospital.API.Models.Entities
 {
-    public class Doсtor : BaseEntity
+    public class Doctor : BaseEntity
     {
         public string FullName { get; set; }
         public DoctorsOffice DoctorsOffice { get; set; }
