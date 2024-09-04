82c66a86269820cabdf6b89d3060f1e1529720c3
kuzda <dmitriy.kuznetsov@elesy.ru>
2024-09-05 02:06:16 +0600

    add: patient, doctor dto classes for edit

---
+++ Hospital.API/DataAccess/DTO/DoctorEditDTO.cs
@@ -0,0 +1,19 @@
+﻿using Hospital.API.Models.Entities;
+
+namespace Hospital.API.DataAccess.DTO
+{
+    public class DoctorEditDTO
+    {
+        public string FullName { get; set; }
+        public Guid DoctorsOfficeId { get; set; }
+        public Guid MedicalSpecialtyId { get; set; }
+        public Guid MedicalCenterId { get; set; }
+        public DoctorEditDTO(string fullName, Guid doctorsOfficeId, Guid medicalSpecialtyId, Guid medicalCenterId)
+        {
+            FullName = fullName;
+            DoctorsOfficeId = doctorsOfficeId;
+            MedicalSpecialtyId = medicalSpecialtyId;
+            MedicalCenterId = medicalCenterId;
+        }
+    }
+}
