dddc9bf522cb9dc4649b1301f32269dffc47d7bf
kuzda <dmitriy.kuznetsov@elesy.ru>
2024-09-05 00:25:59 +0600

    add: main api db context

---
+++ Hospital.API/DataAccess/Contexts/ApiMainContext.cs
@@ -0,0 +1,18 @@
+﻿using Hospital.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.API.DataAccess.Contexts
+{
+    public class ApiMainContext : DbContext
+    {
+        public DbSet<MedicalCenter> MedicalCenters { get; set; }
+        public DbSet<MedicalSpecialty> MedicalSpecialities { get; set; }
+        public DbSet<DoctorsOffice> DoctorsOffices { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doсtor> Doctors { get; set; }
+        public ApiMainContext(DbContextOptions<ApiMainContext> options) : base(options)
+        {
+
+        }
+    }
+}
