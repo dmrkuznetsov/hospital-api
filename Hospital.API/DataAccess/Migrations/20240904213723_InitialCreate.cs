a01f6473b7c8dbd2ac2f7e8caef46c98abb6a015
kuzda <dmitriy.kuznetsov@elesy.ru>
2024-09-05 03:38:04 +0600

    add: initial db migrations

---
+++ Hospital.API/DataAccess/Migrations/20240904213723_InitialCreate.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital.API.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DoctorsOffices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorsOffices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicalCenters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalCenters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicalSpecialities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalSpecialities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Patronymic = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    BirthDate = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Gender = table.Column<int>(type: "INTEGER", nullable: false),
+                    MedicalCenterId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Patients_MedicalCenters_MedicalCenterId",
+                        column: x => x.MedicalCenterId,
+                        principalTable: "MedicalCenters",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FullName = table.Column<string>(type: "TEXT", nullable: true),
+                    DoctorsOfficeId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    MedicalSpecialtyId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    MedicalCenterId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Doctors_DoctorsOffices_DoctorsOfficeId",
+                        column: x => x.DoctorsOfficeId,
+                        principalTable: "DoctorsOffices",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Doctors_MedicalCenters_MedicalCenterId",
+                        column: x => x.MedicalCenterId,
+                        principalTable: "MedicalCenters",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Doctors_MedicalSpecialities_MedicalSpecialtyId",
+                        column: x => x.MedicalSpecialtyId,
+                        principalTable: "MedicalSpecialities",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_DoctorsOfficeId",
+                table: "Doctors",
+                column: "DoctorsOfficeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_MedicalCenterId",
+                table: "Doctors",
+                column: "MedicalCenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_MedicalSpecialtyId",
+                table: "Doctors",
+                column: "MedicalSpecialtyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_MedicalCenterId",
+                table: "Patients",
+                column: "MedicalCenterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.DropTable(
+                name: "DoctorsOffices");
+
+            migrationBuilder.DropTable(
+                name: "MedicalSpecialities");
+
+            migrationBuilder.DropTable(
+                name: "MedicalCenters");
+        }
+    }
+}
